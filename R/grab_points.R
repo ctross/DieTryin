a70bb67ef9402fe76879e1f2588fcb181ec4de1d
ctross <ctross@ucdavis.edu>
2020-07-04 21:54:57 +0200

    Create grab_points.R

---
+++ R/grab_points.R
@@ -0,0 +1,16 @@
+grab_points <- function(path_imgs){
+  imgs <- vector("list",length(path_imgs))
+  locs <- vector("list",length(path_imgs))
+
+ for(i in 1:length(imgs)){
+  imgs[[i]] <- load.image(path_imgs[i])
+
+  loc1<-grabPoint(imgs[[i]])  
+  loc2<-grabPoint(imgs[[i]])  
+  loc3<-grabPoint(imgs[[i]])  
+  loc4<-grabPoint(imgs[[i]])  
+
+  locs[[i]] <- rbind(loc1,loc2,loc3,loc4)
+ }
+ return(locs)
+}
