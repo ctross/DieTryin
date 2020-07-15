9cd790750365e2d12c9903e7c87b0f25832820c1
ctross <ctross@ucdavis.edu>
2020-07-15 19:14:34 +0200

    Create downsize.R

---
+++ R/downsize.R
@@ -0,0 +1,13 @@
+downsize <- function(path=path, scaler=2){
+  path_imgs_big <- paste0(path, "ResultsPhotos/")
+  path_imgs_small <- paste0(path, "ResultsPhotosSmall/")
+  to_read <- list.files(path_imgs_big, full=TRUE)
+  to_save <- list.files(path_imgs_big)
+
+  for(i in 1:length(to_read)){
+   imgRaw <- load.image(to_read[i])
+   img <- resize(imgRaw,round(width(imgRaw)/scaler),round(height(imgRaw)/scaler))
+
+   save.image(img, paste0(path_imgs_small,to_save[i]), quality = 0.9)
+   }
+}
