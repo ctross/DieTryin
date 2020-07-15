62d1ad8716b0c091be06954f4a1e1e294ef62eeb
ctross <ctross@ucdavis.edu>
2020-07-15 19:15:16 +0200

    Update downsize.R

--- R/downsize.R
+++ R/downsize.R
@@ -1,4 +1,5 @@
 downsize <- function(path=path, scaler=2){
+  library(imager)
   path_imgs_big <- paste0(path, "ResultsPhotos/")
   path_imgs_small <- paste0(path, "ResultsPhotosSmall/")
   to_read <- list.files(path_imgs_big, full=TRUE)
