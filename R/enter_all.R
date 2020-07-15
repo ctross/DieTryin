ee7c828168b03c8bc64a89345493371d727d7046
ctross <ctross@ucdavis.edu>
2020-07-15 19:36:46 +0200

    Update enter_all.R

--- R/enter_all.R
+++ R/enter_all.R
@@ -1,3 +1,4 @@
+
 enter_all <- function(path = "C:/Users/Mind Is Moving/Desktop/RICH/", pattern = ".jpg", start = 1, stop = 3, 
     seed = 1, n_frames = 4, n_rows = 5, n_cols = 9, lower_hue_threshold = 210, upper_hue_threshold = 230, colors=c("empty","red"),
     img, locs, focal="CTR", case, thresh=0.25, ordered=NULL, 
@@ -10,7 +11,7 @@ enter_all <- function(path = "C:/Users/Mind Is Moving/Desktop/RICH/", pattern = 
    res <- vector("list",length(img)-1)
 
     i <- 1
-    X <- enter_data(path=path, pattern=pattern, start=start, stop=stop, seed=seed, n_frames=n_frames, n_rows=n_rows, n_cols=n_cols, 
+    X <- auto_enter_data(path=path, pattern=pattern, start=start, stop=stop, seed=seed, n_frames=n_frames, n_rows=n_rows, n_cols=n_cols, 
                     lower_hue_threshold=lower_hue_threshold, upper_hue_threshold=upper_hue_threshold, colors=colors, thresh=thresh,
                     img=img[[i]], locs=locs[[i]], focal=focal, case=case[[i]],clean=NA, ordered=ordered,
                     lower_saturation_threshold=lower_saturation_threshold, 
@@ -22,7 +23,7 @@ enter_all <- function(path = "C:/Users/Mind Is Moving/Desktop/RICH/", pattern = 
     print(paste0("Finished processing image ", "Blank" ))
 
    for(i in 1:(length(img)-1)){
-     res[[i]] <- enter_data(path=path, pattern=pattern, start=start, stop=stop, seed=seed, n_frames=n_frames, n_rows=n_rows, n_cols=n_cols, 
+     res[[i]] <- auto_enter_data(path=path, pattern=pattern, start=start, stop=stop, seed=seed, n_frames=n_frames, n_rows=n_rows, n_cols=n_cols, 
                             lower_hue_threshold=lower_hue_threshold, upper_hue_threshold=upper_hue_threshold, colors=colors, thresh=thresh,
                             img=img[[i+1]], locs=locs[[i+1]], focal=focal, case=case[[i+1]], clean=X[[1]], ordered=ordered,
                             lower_saturation_threshold=lower_saturation_threshold, 
