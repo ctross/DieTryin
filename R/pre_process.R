76aad15b6b3cd366a88094721d8578e2af969436
ctross <ctross@ucdavis.edu>
2020-07-15 19:27:24 +0200

    Update pre_process.R

--- R/pre_process.R
+++ R/pre_process.R
@@ -1,6 +1,6 @@
 pre_process <- function(path=path, ID, GID="Blank", BID=c("A","B")){
  blank_photos_to_read <- paste0(GID, "_", ID, "_",BID,".jpg")
- blank_path <- paste0(path, "ResultsPhotosSmall/", blank_photos_to_read)
+ blank_path <- paste0(path, "/ResultsPhotosSmall/", blank_photos_to_read)
  blank_images <- grab_images(blank_path)
  blank_locs <- grab_points(blank_path)
  return(list(blank_images,blank_locs))
