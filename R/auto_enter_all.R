eda911c697d9e949282afdad3cc74a3fefe58a08
ctross <ctross@ucdavis.edu>
2020-07-17 17:00:04 +0200

    Update auto_enter_all.R

--- R/auto_enter_all.R
+++ R/auto_enter_all.R
@@ -6,7 +6,7 @@
 #' @export
 
 auto_enter_all <- function(path = "C:/Users/Mind Is Moving/Desktop/RICH/", pattern = ".jpg", start = 1, stop = 3, 
-    seed = 1, n_frames = 4, n_rows = 5, n_cols = 9, lower_hue_threshold = 210, upper_hue_threshold = 230, colors=c("empty","red"),
+    seed = 1, n_frames = 4, n_rows = 5, n_cols = 9, lower_hue_threshold = 210, upper_hue_threshold = 230, plot_colors=c("empty","red"),
     img, locs, focal="CTR", case, thresh=0.25, ordered=NULL, 
                      lower_saturation_threshold=0.05, 
                      lower_luminance_threshold=0.05, 
@@ -19,7 +19,7 @@ auto_enter_all <- function(path = "C:/Users/Mind Is Moving/Desktop/RICH/", patte
 
     i <- 1
     X <- auto_enter_data(path=path, pattern=pattern, start=start, stop=stop, seed=seed, n_frames=n_frames, n_rows=n_rows, n_cols=n_cols, 
-                    lower_hue_threshold=lower_hue_threshold, upper_hue_threshold=upper_hue_threshold, colors=colors, thresh=thresh,
+                    lower_hue_threshold=lower_hue_threshold, upper_hue_threshold=upper_hue_threshold, plot_colors=plot_colors, thresh=thresh,
                     img=img[[i]], locs=locs[[i]], focal=focal, case=case[[i]],clean=NA, ordered=ordered,
                     lower_saturation_threshold=lower_saturation_threshold, 
                         lower_luminance_threshold=lower_luminance_threshold, 
@@ -32,7 +32,7 @@ auto_enter_all <- function(path = "C:/Users/Mind Is Moving/Desktop/RICH/", patte
 
    for(i in 1:(length(img)-1)){
      res[[i]] <- auto_enter_data(path=path, pattern=pattern, start=start, stop=stop, seed=seed, n_frames=n_frames, n_rows=n_rows, n_cols=n_cols, 
-                            lower_hue_threshold=lower_hue_threshold, upper_hue_threshold=upper_hue_threshold, colors=colors, thresh=thresh,
+                            lower_hue_threshold=lower_hue_threshold, upper_hue_threshold=upper_hue_threshold, plot_colors=plot_colors, thresh=thresh,
                             img=img[[i+1]], locs=locs[[i+1]], focal=focal, case=case[[i+1]], clean=X[[1]], ordered=ordered,
                             lower_saturation_threshold=lower_saturation_threshold, 
                         lower_luminance_threshold=lower_luminance_threshold, 
