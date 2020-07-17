2073106bc8e1b416a02470174d3013bafef77029
ctross <ctross@ucdavis.edu>
2020-07-17 19:52:14 +0200

    Update shatter.R

--- R/shatter.R
+++ R/shatter.R
@@ -4,19 +4,18 @@
 #' @param 
 #' x An object.
 #' @export
-
-
 shatter <- function(image, locations, n_rows = 5, n_cols = 9, lower_hue_threshold=lower_hue_threshold, 
                     upper_hue_threshold=upper_hue_threshold, lower_saturation_threshold=lower_saturation_threshold, 
                      lower_luminance_threshold=lower_luminance_threshold, 
                      upper_luminance_threshold=upper_luminance_threshold, 
                      border_size=border_size,
                      iso_blur=iso_blur,
-                     histogram_balancing=histogram_balancing){
+                     histogram_balancing=histogram_balancing,
+                     direction="backward"){
  # shatter takes in an image file and corner locations, splits the file into individual images, 
  # runs the classifier, and exports results and images
  results <- array(NA, c(n_rows, n_cols, length(lower_hue_threshold)))
- pruned_image <- extractor(image, locations, histogram_balancing=histogram_balancing) 
+ pruned_image <- extractor(image, locations, histogram_balancing=histogram_balancing, direction=direction) 
  rows_image <- imsplit(pruned_image,"y",n_rows)
  for(i in 1:n_rows){
  slice_image<- imsplit(rows_image[[i]],"x",n_cols)  
