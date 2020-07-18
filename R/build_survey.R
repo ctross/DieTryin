1e5c5fe687d490c64ad7d576c6112c52c8c2f106
ctross <ctross@ucdavis.edu>
2020-07-19 00:25:50 +0200

    Update build_survey.R

--- R/build_survey.R
+++ R/build_survey.R
@@ -1,72 +1,77 @@
 #' A function to build LaTeX survey tool for RICH economic games
 #'
-#' This function allows you to speed up data collection and photo randomization. Simply set a path the main folder.  Set the number of panels, and the number of rows and cols per panel. Then run the function. This function relies on 'xtable'. See details below.
+#' This function allows you to speed up data collection and photo randomization. Simply set a path the main folder.  
+#' Set the number of panels, and the number of rows and cols per panel. Then run the function. This function relies on 'xtable' to create a survey tool, and compile to PDF using LaTeX.
+#' The user must have a LaTeX build on the system path.
 #' @param 
 #' path Full path to main folder.
 #' @param 
-#' pattern File extension of photos. Should be .jpg or .JPG. 
+#' pattern File extension of photos. Should be ".jpg" or ".JPG". 
 #' @param 
-#' start Location of start of PID in file name. If files are saved as XXX.jpg for example, this is 1.
+#' start Location of start of PID in file name. If files are saved as "XXX.jpg" for example, this is 1.
 #' @param 
-#' stop Location of end of PID in file name. ... and this is 3.  
+#' stop Location of end of PID in file name. If files are saved as "XXX.jpg" for example, this is 3.
 #' @param 
 #' seed A seed for the random number generator to sort the order of photos in the array. 
 #' @param 
-#' frames Number of frames/panels/blocks of photos to be output. I use four big panels and randomize order at each game.
+#' n_frames Number of frames/panels/blocks of photos to be output. I use four big panels and randomize order at each game.
 #' @param 
-#' rows Number of rows per panel. With 7cm x 10cm photos, I use five rows of photos per panel.
+#' n_rows Number of rows per panel. With 7cm x 10cm photos, I use five rows of photos per panel.
 #' @param 
-#' cols Number of rows per panel. With 7cm x 10cm photos, I use six to eight cols of photos per panel.
+#' n_cols Number of cols per panel. With 7cm x 10cm photos, I use six to eight cols of photos per panel.
+#' @param 
+#' ordered A list of IDs in explicit order. Can be used to overide random sorting.
 #' @export
 #' @examples
 #' \dontrun{
-#'   build_survey(path=path,pattern=".jpg",start=1,stop=3,frames=4,seed=1, rows=5, cols=8)
+#' build_survey(path=path, pattern=".jpg", start=1, stop=3, n_frames=2, n_rows=4, n_cols=5, seed=1, ordered = sorted_ids)
 #'                    }
   
-build_survey <- function(path=path, pattern=".jpg", start=1, stop=3,
-                         n_frames=4, n_rows=5, n_cols=8, seed=1, ordered = NULL ){
-                         
-path_out<-paste0(path,"/Survey")
-require(xtable)
-require(readr)
-IDS <- substr(list.files(paste0(path,"/","StandardizedPhotos"), pattern, full.names=FALSE), start = start, stop = stop) # Load IDs from photos
+build_survey = function(path, pattern=".jpg", start=1, stop=3, n_frames=4, n_rows=5, n_cols=8, seed=1, ordered = NULL ){
+ require(xtable)
+ require(readr)  
+  
+ path_out = paste0(path,"/Survey")
 
-L <- length(IDS)
+ IDS = substr(list.files(paste0(path,"/","StandardizedPhotos"), pattern, full.names=FALSE), start = start, stop = stop) # Load IDs from photos
 
-if( L> n_frames*n_rows*n_cols){
-stop("ID vector exceeds the product of n_frames*n_rows*n_cols")}
-else{      
-set.seed(seed)
+ L = length(IDS)
+
+ if( L> n_frames*n_rows*n_cols){
+  stop("ID vector exceeds the product of n_frames*n_rows*n_cols")}
+   else{      
+  set.seed(seed)
 
   if(is.null(ordered)){
-  SortedIDS <- c(IDS[order(runif(length(IDS),0,1))])
+  SortedIDS = c(IDS[order(runif(length(IDS),0,1))])
     }
   
   else{
-    SortedIDS <- ordered
+  SortedIDS = ordered
   }
 
-SortedIDS<-c(SortedIDS,rep("~~~~",(n_frames*n_rows*n_cols - L)) )
+SortedIDS = c(SortedIDS,rep("~~~~",(n_frames*n_rows*n_cols - L)) )
 
- X <- matrix(paste0("\\LARGE \\color{gray}",SortedIDS), nrow=n_rows,ncol=n_frames*n_cols,byrow=FALSE)
- x <- vector("list",n_frames)
+ X = matrix(paste0("\\LARGE \\color{gray}",SortedIDS), nrow=n_rows,ncol=n_frames*n_cols,byrow=FALSE)
+ x = vector("list",n_frames)
  
  for(i in 1:n_frames)
- x[[i]] <- xtable_custom(X[,c(1:n_cols)+n_cols*(i-1)])
+ x[[i]] = xtable_custom(X[,c(1:n_cols)+n_cols*(i-1)])
  
-Code <- c()
-Code[1] <- read_file(paste0(path,"/Survey/","header.txt"))
-Code[2] <- seed
-Code[3] <- " \\\\[12pt] \\end{fshaded}"
+Code = c()
+Code[1] = read_file(paste0(path,"/Survey/","header.txt"))
+Code[2] = seed
+Code[3] = " \\\\[12pt] \\end{fshaded}"
+     
 for(i in 1:n_frames)
-Code[3+i] <- print(x[[i]],include.rownames=FALSE,include.colnames=FALSE,hline.after=0:nrow(x[[i]]), sanitize.text.function = identity)
-Code[n_frames+4] <- " \\end{document}"
+Code[3+i] = print(x[[i]],include.rownames=FALSE,include.colnames=FALSE,hline.after=0:nrow(x[[i]]), sanitize.text.function = identity)
+Code[n_frames+4] = " \\end{document}"
 
 write(Code, file = paste0(path_out,"/","Survey.tex"), append = FALSE)
    }
   
-  wd1<-getwd()   
-wd2<-paste0(path_out,"/")
+wd1 = getwd()   
+wd2 = paste0(path_out,"/")
 setwd(wd2)
 tools::texi2dvi("Survey.tex", pdf = TRUE, clean = TRUE)
 setwd(wd1)
