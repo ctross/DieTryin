3cec87139b3d0185f3802ac3c95e12ac2b2fd7b9
ctross <ctross@ucdavis.edu>
2020-07-18 10:15:00 +0200

    Update pre_process.R

--- R/pre_process.R
+++ R/pre_process.R
@@ -5,10 +5,10 @@
 #' x An object.
 #' @export
 
-pre_process <- function(path=path, ID, GID="Blank", BID=c("A","B")){
+pre_process <- function(path=path, ID, GID="Blank", BID=c("A","B"), pre_processed=FALSE){
  blank_photos_to_read <- paste0(GID, "_", ID, "_",BID,".jpg")
  blank_path <- paste0(path, "/ResultsPhotosSmall/", blank_photos_to_read)
  blank_images <- grab_images(blank_path)
- blank_locs <- grab_points(blank_path)
+ blank_locs <- grab_points(blank_path, pre_processed=pre_processed)
  return(list(blank_images,blank_locs))
 }
