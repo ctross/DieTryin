74ebf71dd09b01b70821f9d1f9ad765b47c14c49
ctross <ctross@ucdavis.edu>
2020-07-18 10:13:08 +0200

    Update grab_points.R

--- R/grab_points.R
+++ R/grab_points.R
@@ -4,13 +4,12 @@
 #' @param 
 #' x An object.
 #' @export
-
-grab_points <- function(path_imgs){
+grab_points <- function(path_imgs, pre_processed=FALSE){
   imgs <- vector("list",length(path_imgs))
   locs <- vector("list",length(path_imgs))
-
- for(i in 1:length(imgs)){
-  imgs[[i]] <- load.image(path_imgs[i])
+  if(pre_processed==FALSE){
+   for(i in 1:length(imgs)){
+    imgs[[i]] <- load.image(path_imgs[i])
 
      if(dim(imgs[[i]])[1]>dim(imgs[[i]])[2]){
     imgs[[i]]<-imrotate(imgs[[i]],90)}
@@ -21,6 +20,15 @@ grab_points <- function(path_imgs){
   loc4<-grabPoint(imgs[[i]])  
 
   locs[[i]] <- rbind(loc1,loc2,loc3,loc4)
+   }}
+ else{
+  loc1<-c(0, dim(imgs[[i]])[2])  
+  loc2<-c(dim(imgs[[i]])[1], dim(imgs[[i]])[2])  
+  loc3<-c(dim(imgs[[i]])[1], 0)  
+  loc4<-c(0,0)  
+
+  locs[[i]] <- rbind(loc1,loc2,loc3,loc4)
  }
+
  return(locs)
 }
