5e8a866d46916fd629db8f1f237beb33e2067ca3
ctross <ctross@ucdavis.edu>
2020-07-18 11:12:24 +0200

    Update grab_points.R

--- R/grab_points.R
+++ R/grab_points.R
@@ -8,30 +8,31 @@
 grab_points <- function(path_imgs, pre_processed=FALSE){
   imgs <- vector("list",length(path_imgs))
   locs <- vector("list",length(path_imgs))
-  if(pre_processed==FALSE){
+  
    for(i in 1:length(imgs)){
     imgs[[i]] <- load.image(path_imgs[i])
 
-     if(dim(imgs[[i]])[1]>dim(imgs[[i]])[2]){
-    imgs[[i]]<-imrotate(imgs[[i]],90)}
+     if(pre_processed==FALSE){
 
-  loc1<-grabPoint(imgs[[i]])  
-  loc2<-grabPoint(imgs[[i]])  
-  loc3<-grabPoint(imgs[[i]])  
-  loc4<-grabPoint(imgs[[i]])  
+      if(dim(imgs[[i]])[1]>dim(imgs[[i]])[2]){
+        imgs[[i]]<-imrotate(imgs[[i]],90)}
 
-  locs[[i]] <- rbind(loc1,loc2,loc3,loc4)
-   }}
- else{
-  for(i in 1:length(imgs)){
-  loc1<-c(0, dim(imgs[[i]])[2])  
-  loc2<-c(dim(imgs[[i]])[1], dim(imgs[[i]])[2])  
-  loc3<-c(dim(imgs[[i]])[1], 0)  
-  loc4<-c(0,0)  
+      loc1<-grabPoint(imgs[[i]])  
+      loc2<-grabPoint(imgs[[i]])  
+      loc3<-grabPoint(imgs[[i]])  
+      loc4<-grabPoint(imgs[[i]])  
 
-  locs[[i]] <- rbind(loc1,loc2,loc3,loc4)
+       locs[[i]] <- rbind(loc1,loc2,loc3,loc4)
+        }
+         else{
+  
+       loc1<-c(0, dim(imgs[[i]])[2])  
+       loc2<-c(dim(imgs[[i]])[1], dim(imgs[[i]])[2])  
+       loc3<-c(dim(imgs[[i]])[1], 0)  
+       loc4<-c(0,0)  
+            }
+        locs[[i]] <- rbind(loc1,loc2,loc3,loc4)
    }
- }
 
  return(locs)
 }
