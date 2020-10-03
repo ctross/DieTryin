1fdb7c74c141ffaea617a57b8acee50bf710b8df
ctross <ctross@ucdavis.edu>
2020-10-03 14:01:31 +0200

    Update pre_process.R

--- R/pre_process.R
+++ R/pre_process.R
@@ -8,7 +8,7 @@
 #' @param 
 #' GID ID for case/game/question.
 #' @param 
-#' BID ID code used to distinguish photographs of the different board panels/frames from the same person and game.
+#' PID ID code used to distinguish photographs of the different board panels/frames from the same person and game.
 #' @param 
 #' pre_processed Are photographs pre-processed such that image correction steps can be skipped? If FALSE, then user must pre-process images using DieTryings tools. If TRUE game board photographs must be cropped and unskewed. 
 #' Some Android and IOS apps, like Tiny Scanner, provide a means of producing such photographs of the game boards at the time of data collection.
@@ -16,11 +16,11 @@
 #' @examples
 #' \dontrun{
 #' filled = vector("list", 26)
-#' filled[[1]] = pre_process(path=path, ID="QQQ", GID="Blank", BID=c("A","B"))
+#' filled[[1]] = pre_process(path=path, ID="QQQ", GID="Blank", PID=c("A","B"))
 #'                    }
 
-pre_process <- function(path, ID, GID="Blank", BID=c("A","B"), pre_processed=FALSE){
- blank_photos_to_read = paste0(GID, "_", ID, "_", BID, ".jpg")
+pre_process <- function(path, ID, GID="Blank", PID=c("A","B"), pre_processed=FALSE){
+ blank_photos_to_read = paste0(GID, "_", ID, "_", PID, ".jpg")
  blank_path = paste0(path, "/ResultsPhotosSmall/", blank_photos_to_read)
  blank_images = grab_images(blank_path)
  blank_locs = grab_points(blank_path, pre_processed=pre_processed)
