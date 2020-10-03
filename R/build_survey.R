c54386d3540b8a467e49f0b0ba9ef8bc720397cd
ctross <ctross@ucdavis.edu>
2020-10-03 13:57:42 +0200

    Update build_survey.R

--- R/build_survey.R
+++ R/build_survey.R
@@ -14,7 +14,7 @@
 #' @param 
 #' seed A seed for the random number generator to sort the order of photos in the array. 
 #' @param 
-#' n_frames Number of frames/panels/blocks of photos to be output. I use four big panels and randomize order at each game.
+#' n_panels Number of frames/panels/blocks of photos to be output. I use four big panels and randomize order at each game.
 #' @param 
 #' n_rows Number of rows per panel. With 7cm x 10cm photos, I use five rows of photos per panel.
 #' @param 
@@ -24,10 +24,10 @@
 #' @export
 #' @examples
 #' \dontrun{
-#' build_survey(path=path, pattern=".jpg", start=1, stop=3, n_frames=2, n_rows=4, n_cols=5, seed=1, ordered = sorted_ids)
+#' build_survey(path=path, pattern=".jpg", start=1, stop=3, n_panels=2, n_rows=4, n_cols=5, seed=1, ordered = sorted_ids)
 #'                    }
   
-build_survey = function(path, pattern=".jpg", start=1, stop=3, n_frames=4, n_rows=5, n_cols=8, seed=1, ordered = NULL ){
+build_survey = function(path, pattern=".jpg", start=1, stop=3, n_panels=4, n_rows=5, n_cols=8, seed=1, ordered = NULL ){
  require(xtable)
  require(readr)  
  require(tools) 
@@ -38,8 +38,8 @@ build_survey = function(path, pattern=".jpg", start=1, stop=3, n_frames=4, n_row
 
  L = length(IDS)
 
- if( L> n_frames*n_rows*n_cols){
-  stop("ID vector exceeds the product of n_frames*n_rows*n_cols")}
+ if( L> n_panels*n_rows*n_cols){
+  stop("ID vector exceeds the product of n_panels*n_rows*n_cols")}
    else{      
   set.seed(seed)
 
@@ -51,12 +51,12 @@ build_survey = function(path, pattern=".jpg", start=1, stop=3, n_frames=4, n_row
   SortedIDS = ordered
   }
 
-SortedIDS = c(SortedIDS,rep("~~~~",(n_frames*n_rows*n_cols - L)) )
+SortedIDS = c(SortedIDS,rep("~~~~",(n_panels*n_rows*n_cols - L)) )
 
- X = matrix(paste0("\\LARGE \\color{gray}",SortedIDS), nrow=n_rows,ncol=n_frames*n_cols,byrow=FALSE)
- x = vector("list",n_frames)
+ X = matrix(paste0("\\LARGE \\color{gray}",SortedIDS), nrow=n_rows,ncol=n_panels*n_cols,byrow=FALSE)
+ x = vector("list",n_panels)
  
- for(i in 1:n_frames)
+ for(i in 1:n_panels)
  x[[i]] = xtable_custom(X[,c(1:n_cols)+n_cols*(i-1)])
  
 Code = c()
@@ -64,7 +64,7 @@ Code[1] = read_file(paste0(path,"/Survey/","header.txt"))
 Code[2] = seed
 Code[3] = " \\\\[12pt] \\end{fshaded}"
      
-for(i in 1:n_frames)
+for(i in 1:n_panels)
 Code[3+i] = print(x[[i]],include.rownames=FALSE,include.colnames=FALSE,hline.after=0:nrow(x[[i]]), sanitize.text.function = identity)
 Code[n_frames+4] = " \\end{document}"
 
