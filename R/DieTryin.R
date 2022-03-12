c8fb332c2965292587f59351c85dec2e8d8a4aab
ctross <ctross@ucdavis.edu>
2022-03-12 14:30:24 -0500

    Add files via upload

---
+++ R/DieTryin.R
@@ -0,0 +1,8 @@
+#' DieTryin: Social network collection and automated entry using R
+#'
+#' DieTryin is an R package designed to facilitate the collection of roster-based network data, 
+#' and to run network-structured economic games—such as Recipient Identity-Conditioned Heuristics (RICH) games.
+#'
+#' @docType package
+#' @name DieTryin
+NULL
